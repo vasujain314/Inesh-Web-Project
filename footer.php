581be03b18c5206bb2a48c57b4ede5ae2957cf03
vasujain314 <jain.vasu509@gmail.com>
2019-03-25 17:20:18 +0530

    header-footer

---
+++ footer.php
@@ -0,0 +1,72 @@
+  <!DOCTYPE html>
+  <html>
+  <body>
+  
+
+
+  <section class="subscribe-section">
+        <div class="container">
+            <div class="text-area">
+                <div class="section-title text-center">
+                    <h2>Subscribe Newsletter</h2>
+                    <p>Subscribe and get hold of our all the new services and stunning discounts</p>
+                </div>
+                <form method="post" action="contact.html">
+                    <div class="row">
+                        <div class="col-lg-9">
+                            <div class="form-group">                        
+                                <input type="email" name="email" value="" placeholder="Enter your Email" required="">
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="form-group">                        
+                                <button style="color: white; background-color:#E13737;border: 1px solid #E13737" type="submit" class="btn-style-one">SUBMIT</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+                <ul class="social-links text-center">
+                    <li><a href=" https://www.facebook.com/ineshenterprises/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/ineshenterpris2"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.instagram.com/inesh_enterprises/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href=" https://www.linkedin.com/company/ineshenterprises/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UC-8foFDp6goCr3oyLbovLOw"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>                
+        </div>
+    </section>
+    <!--End Subscribe Section-->
+
+<div class="container">
+    <div class="row">
+        
+        <div class="col-sm-3 col-xs-3" align="center"><img class="img-fluid" style="width: 120px; " src="img/footer/ISO Certified.png">   </div>
+        <div class="col-sm-3 col-xs-3" align="center"><img style="width: 120px;  margin-left: 0px;" src="img/footer/Google Certified.jpg"></div>
+        <div class="col-sm-3 col-xs-3" align="center"> <img style="width: 120px;  margin-left: 0px;" src="img/footer/Microsoft partner Network.png"></div>
+        <div class="col-sm-3 col-xs-3" align="center"> <img style="width: 120px;  margin-left: 0px;" src="img/footer/Google-4.9.png">  </div>
+        <br />
+    </div>
+</div>
+    <!--main-footer-->
+    <footer class="footer-bottom">
+        <div class="container clearfix">
+            <div class="row">
+
+                <div class="col-sm-1"><a href="index.html"><img width="80px" style="margin-top:0px" src="images/logo.png" alt=""></a></div>
+            <div class="col-sm-1"></div>
+                <div class="col-sm-2"><a style="font-size: 13px;" href="terms.html">Terms & conditions</a> </div>
+                <div class="col-sm-2"><a style="font-size: 13px;" href="disclaimer.html">Disclaimer</a></div>
+                <div class="col-sm-2" align="left"><a style="font-size: 13px;" href="privacy.html"> Privacy policy</a></div>
+                <div class="col-sm-2"><a style="font-size: 13px;" href="refund.html"> Refund & Cancellation policy</a></div>
+
+              <div class="col-sm-2">
+                <p>Eventre &copy; 2017 All Right Reserved</p>
+               </div>
+
+                 
+            </div>
+            
+           
+        </div>
+    </footer>
+</body></html>
---
+++ header.php
@@ -0,0 +1,133 @@
+ <!DOCTYPE html>
+ <body>
+  <div class="header-top"></div>
+
+  <header class="main-header sticky-header">
+        <div class="container-fluid clearfix">
+            <div class="logo" >
+                <figure>
+                    <a href="index.html"  ><img width="150px" style="margin-top:10px"  src="images/logo.png" alt=""></a>
+                </figure>
+            </div>
+            <nav class="main-menu">
+                <div class="navbar-header">
+                    <!-- Toggle Button -->      
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>           
+                <div class="navbar-collapse collapse clearfix">
+                    <ul class="navigation clearfix">
+                        <li class="current"></li> 
+                        <li><a href="about.html">About</a></li>
+                        <li class="dropdown"><a href="service.html" onclick="window.location='service.html'">Web Services</a>
+                            <ul>
+                                
+                                <li><a href="web-dev.html">Web Designing & Development</a></li>
+                                <li><a href="ecomm.html">E-commerce Development</a></li>
+                                <li><a href="mobile.html">Mobile Application Development</a></li>
+                                <li><a href="graphic.html">Graphics & Logo Desigining</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="technology.html">Technology</a></li>
+                        
+                        <li class="dropdown"><a href="marketing.html" onclick="window.location='marketing.html'">Marketing</a>
+                            <ul>
+                                <li><a href="seo-lead.html">SEO & Online Marketing</a></li>
+                                <li><a href="social-media.html">Social Media Marketing</a></li>
+                                <li><a href="content-mkt.html">Content Marketing</a></li>
+                                 <li><a href="google-adwords.html">PPC/Google AdWords</a></li>
+                                  <li><a href="reputation-mgt.html">Online Reputation Management</a></li>
+                                  <li><a href="lead.html">Lead Generation</a></li>
+                                   <li><a href="video-mkt.html">Video Marketing</a></li>
+                                   <li><a href="email-marketing.html">Email Marketing</a></li>
+
+                            </ul>
+                        </li> 
+                         <li><a href="ourwork.html">Our Work</a></li>                  
+                        <li><a href="contact.html">Contact Us</a></li>
+                    </ul>
+                </div>                    
+            </nav>
+            <div class="link-button">
+                <div class="search_option">
+                    <a href="#"><span class="icon fa fa-search"></span></a>
+                </div>
+                <ul class="social-links">
+                    <li><a href=" https://www.facebook.com/ineshenterprises/"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/ineshenterpris2"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.instagram.com/inesh_enterprises/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                    <li><a href=" https://www.linkedin.com/company/ineshenterprises/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UC-8foFDp6goCr3oyLbovLOw"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+<div id="search-popup" class="search-popup">
+    <div class="close-search theme-btn"><span class="fa fa-times" aria-hidden="true"></span></div>
+    <div class="popup-inner">
+        <div class="search-form">
+            <form method="post" action="index.html">
+                <div class="form-group">
+                    <fieldset>
+                        <input type="search" class="form-control" name="search-input" value="" placeholder="Search Here" required >
+                        <input type="submit" value="Search" class="theme-btn">
+                    </fieldset>
+                </div>
+            </form>
+            <br>
+            <h3>Recent Search Keywords</h3>
+            <ul class="recent-searches">
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Web Development</a></li>
+                <li><a href="#">SEO</a></li>
+                <li><a href="#">Logistics</a></li>
+                <li><a href="#">Freedom</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!--Scroll to top-->
+<!-- <div class="scroll-to-top scroll-to-target" data-target=".header-top"><span class="icon fa fa-angle-up"></span></div> -->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/html5lightbox.js"></script>
+<script src="js/isotope.js"></script> 
+
+
+<!-- revolution slider js -->
+<script src="assets/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/validate.js"></script>
+<script src="js/wow.js"></script>
+
+<!-- gmap helper -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHzPSV2jshbjI8fqnC_C4L08ffnj5EN3A"></script>
+<!--gmap script-->
+<script src="js/gmaps.js"></script>
+<script src="js/map-helper.js"></script>
+
+<!-- bxslider -->
+<script src="js/jquery.appear.js"></script>
+<script src="js/jquery.countTo.js"></script>
+
+<script src="js/script.js"></script>
+
+</body></html>
